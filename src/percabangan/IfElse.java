f730a679e7722500cd5708f4254757335d2d03d0
rahmatulaina <rahmatulaina28@gmail.com>
2020-05-02 15:24:06 +0800

    If dengan Condition Bernilai False & Condition True

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -7,7 +7,7 @@ public class IfElse {
 
         if(uangDiDompet<totalBelanja){
             System.out.println("Uang kurang, kurangi jajan anda");
-        }else if(uangDiDompet<totalBelanja){
+        }else if(uangDiDompet>totalBelanja){
             double angsul = uangDiDompet-totalBelanja;
             System.out.println("Uang cukup, angsul : "+angsul);
         }else{
